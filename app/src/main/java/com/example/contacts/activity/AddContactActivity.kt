71f799f990ffd6b5ab67f37e256631a8886446b0
Ashish Gupta <ashishg0926@gmail.com>
2021-10-19 11:23:51 +0530

    created AddContactActivity for adding new contact

---
+++ app/src/main/java/com/example/contacts/activity/AddContactActivity.kt
@@ -0,0 +1,12 @@
+package com.example.contacts.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.contacts.R
+
+class AddContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_contact)
+    }
+}
